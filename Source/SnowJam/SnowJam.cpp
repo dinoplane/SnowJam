f757d2073321e0587f91d194df96f6a4ad2c654f
dinoplane <isacsac2017@gmail.com>
2024-01-02 16:28:24 -0800

    init

---
+++ Source/SnowJam/SnowJam.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SnowJam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SnowJam, "SnowJam" );
